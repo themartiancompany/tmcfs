7db787f0fa31966dc0cd078a0698f0474453d1c9
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-06-21 03:20:48 +0000

    mm

---
+++ webpack.config.cjs
@@ -0,0 +1,90 @@
+const
+  _path =
+    require(
+      'path');
+const
+  _output_dir =
+    _path.resolve(
+      __dirname);
+const
+  _input_file_name =
+    "fs";
+const
+  _output_file_name =
+    `${_input_file_name}.js`;
+const
+  _input_file_path =
+    `./${_input_file_name}/${_input_file_name}`;
+const
+  _node_fs_ignore =
+  { resourceRegExp:
+      /^node:fs$/ };
+const
+  _yargs_ignore =
+  { resourceRegExp:
+      /^yargs$/ };
+const
+  _yargs_helpers_ignore = {
+    resourceRegExp:
+      /^yargs\/helpers$/
+};
+const
+  _webpack =
+    require(
+     "webpack");
+const
+  _ignore_plugin =
+    _webpack.IgnorePlugin; 
+const
+  _node_fs_ignore_plugin =
+    new _ignore_plugin(
+          _node_fs_ignore);
+const
+  _yargs_ignore_plugin =
+    new _ignore_plugin(
+          _yargs_ignore);
+const
+  _yargs_helpers_ignore_plugin =
+    new _ignore_plugin(
+          _yargs_helpers_ignore);
+const
+  _output =
+  { path:
+      _output_dir,
+    filename:
+      _output_file_name };
+module.exports = {
+  entry:
+    _input_file_path,
+  output:
+    _output,
+  optimization: {
+    moduleIds: 'deterministic',
+  },
+  resolve: {
+    alias: {
+      "path":
+        _path.resolve(
+          __dirname,
+          'node_modules/path/mod.js'),
+      "web-worker":
+        _path.resolve(
+          __dirname,
+          'node_modules/web-worker/dist/browser/index.cjs'),
+    },
+    fallback: {
+      "node:fs":
+        false,
+      "utils":
+        false
+    },
+  },
+  externals:
+    { yargs:
+        'yargs' },
+  plugins: [
+    _node_fs_ignore_plugin,
+    _yargs_ignore_plugin,
+    _yargs_helpers_ignore_plugin
+  ]
+};
